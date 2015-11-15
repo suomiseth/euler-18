57ddecb8874784a487ab23dca6ea93c58c9a5c01
suomiseth <suomiseth@gmail.com>
2015-11-15 04:40:04 -0500

    all done... took me a long time

--- right-question.rb
+++ right-question.rb
@@ -4,52 +4,36 @@ class MaximumPath
 
   def initialize
     @array = [
-      [75],
-      [95, 64],
-      [17, 47, 82],
-      [18, 35, 87, 10],
-      [20, 04, 82, 47, 65],
-      [19, 01, 23, 75, 03, 34],
-      [88, 02, 77, 73, 07, 63, 67],
-      [99, 65, 04, 28, 06, 16, 70, 92],
-      [41, 41, 26, 56, 83, 40, 80, 70, 33],
-      [41, 48, 72, 33, 47, 32, 37, 16, 94, 29],
-      [53, 71, 44, 65, 25, 43, 91, 52, 97, 51, 14],
-      [70, 11, 33, 28, 77, 73, 17, 78, 39, 68, 17, 57],
-      [91, 71, 52, 38, 17, 14, 91, 43, 58, 50, 27, 29, 48],
+      [04, 62, 98, 27, 23, 9, 70, 98, 73, 93, 38, 53, 60, 04, 23],
       [63, 66, 04, 68, 89, 53, 67, 30, 73, 16, 69, 87, 40, 31],
-      [04, 62, 98, 27, 23, 9, 70, 98, 73, 93, 38, 53, 60, 04, 23]
+      [91, 71, 52, 38, 17, 14, 91, 43, 58, 50, 27, 29, 48],
+      [70, 11, 33, 28, 77, 73, 17, 78, 39, 68, 17, 57],
+      [53, 71, 44, 65, 25, 43, 91, 52, 97, 51, 14],
+      [41, 48, 72, 33, 47, 32, 37, 16, 94, 29],
+      [41, 41, 26, 56, 83, 40, 80, 70, 33],
+      [99, 65, 04, 28, 06, 16, 70, 92],
+      [88, 02, 77, 73, 07, 63, 67],
+      [19, 01, 23, 75, 03, 34],
+      [20, 04, 82, 47, 65],
+      [18, 35, 87, 10],
+      [17, 47, 82],
+      [95, 64],
+      [75]
     ]
-    @answer = []
+    @answer = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
   end
 
   def maximum_path_sum
-    row = 0
-    i = 0
-    @array.each do |arr|
-      @answer << arr[i]
-      i += 1 unless down?(row, i)
-      row += 1
-    end
-    @answer.inject(:+)
-  end
-
-  def down?(row, i)
-    # this isn't comparing the right numbers b/c it needs to shift and pop... 
-    # i think that will be easier.... then it will always compare the first
-    # index to the last index
-    if @array[row + 1]
-      sacrifice = @array[(row + 1)..@array.length].map do |arr|
-        binding.pry
-        arr[i] - (arr[(i * -1) - 1])
+    @array.each.with_index do |arr, i|
+      arr.each.with_index do |n, j|
+        left = @array[i][j] + @answer[j]
+        right = @array[i][j] + @answer[j + 1]
+        left > right ? @answer[j] = left : @answer[j] = right
       end
-      (sacrifice.max + sacrifice.min) > 0 ? true : false
-    else
-      # pick the last biggest number
     end
+    @answer.first
   end
 end
-
 
 puts "crunching the numbers"
 puts MaximumPath.new.maximum_path_sum
